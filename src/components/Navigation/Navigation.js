c3da73b6d569e1db69af044fcfa35f236769905c
dhanvanthshetty <peopleliketools@gmail.com>
2024-06-02 23:28:40 +0530

    "Updated package-lock.json, package.json, and src files to include new dependencies and components, removed logo.svg, and modified styles and layouts."

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+ const Navigation = () => {
+     return (
+     <nav style={{display:"flex",justifyContent:"flex-end"}}>
+    <p className="f3 link dim black underline pointer">Sign out</p>
+
+         </nav>
+     )
+ }
+ export default Navigation
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+ const Rank = () => {
+     return (
+   <div>
+     <div className="white f1"> {"#1"} </div> <div className="white f1">
+                </div></div>
+     )
+ }
+ export default Rank;
